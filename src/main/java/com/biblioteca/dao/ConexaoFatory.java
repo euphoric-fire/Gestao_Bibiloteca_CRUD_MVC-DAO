8b6226edf3c605dbf4a3ebf9ca1eaf699e0aaa54
euphoric-fire <alvaroeduardo3002@gmail.com>
2026-07-17 21:44:17 +0200

    criada a conexao do projecto com o banco de dados

---
+++ src/main/java/com/biblioteca/dao/ConexaoFatory.java
@@ -0,0 +1,21 @@
+package com.biblioteca.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoFatory {
+    private static final String USUARIO = "root";
+    private static final String SENHA = "Alvaro3002";
+    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao conectar ao banco de dados ", e);
+        }
+
+    }
+}
